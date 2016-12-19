eb6aca8e5987945911da165ec91f087decbc4fb6
ToanDK <dkt204@gmail.com>
2016-12-19 15:29:58 +0700

    add unit tests

--- B4USwiftTests/MapRoutingViewModelTests.swift
+++ B4USwiftTests/MapRoutingViewModelTests.swift
@@ -8,10 +8,11 @@
 
 import Quick
 import Nimble
+import CoreLocation
 
 class MapRTViewModelSpec : QuickSpec {
     override func spec() {
-        let viewModel = HomeViewModel()
+        var viewModel = HomeViewModel()
         
         describe("Initialize") {
             it("has everything you need to get started") {
@@ -30,84 +31,49 @@ class MapRTViewModelSpec : QuickSpec {
 //            }
         }
         
-        describe("reverse direction") { 
+        describe("reverse direction") {
             let stopPlace = DTPlace()
             let startPlace = DTPlace()
-            viewModel.startPlace = startPlace
-            viewModel.stopPlace = stopPlace
-//            it("Should assert that reverse start and stop place is ok") {
-//                let resultSignal = viewModel.reverseDirectionCommand?.execute(nil)
-//                _ = try resultSignal?.asynchronouslyWaitUntilCompleted()
-////                expect(success).to(beTrue())
-//                
-//            }
+            stopPlace.coordinate = CLLocationCoordinate2DMake(20.972324, 105.884813)
+            startPlace.coordinate = CLLocationCoordinate2DMake(21.001315, 105.817504)
+            let viewModel2 = HomeViewModel()
+            viewModel2.startPlace = startPlace
+            viewModel2.stopPlace = stopPlace
+            it("Should assert that reverse start and stop place is ok") {
+                _ = try! viewModel2.reverseDirectionCommand!.execute(nil).asynchronouslyWaitUntilCompleted()
+//                expect(success).to(beTrue())
+                expect(viewModel2.startPlace).to(equal(stopPlace))
+                expect(viewModel2.stopPlace).to(equal(startPlace))
+            }
+        }
+        
+        describe("Load map routing") { 
+            let stopPlace = DTPlace()
+            let startPlace = DTPlace()
+            stopPlace.coordinate = CLLocationCoordinate2DMake(20.972324, 105.884813)
+            startPlace.coordinate = CLLocationCoordinate2DMake(21.001315, 105.817504)
             
+            viewModel = HomeViewModel()
             
+            it ("Should assert that encodedRoutingPoints is found") {
+                viewModel.encodedRoutingPoints = NO_ROUTE_KEY
+                viewModel.startPlace = startPlace
+                viewModel.stopPlace = stopPlace
+                let resultSignal = viewModel.loadingMapRoutingSignal
+                _ = try! resultSignal?.asynchronouslyWaitUntilCompleted()
+                expect(viewModel.mapBounds).to(beTruthy())
+                expect(viewModel.encodedRoutingPoints).notTo(equal(NO_ROUTE_KEY))
+            }
+            
+            it ("Should assert that encodedRoutingPoints is NO_ROUTE_KEY") {
+                viewModel.transportMode = "Transit"
+                stopPlace.coordinate = CLLocationCoordinate2DMake(34.112603, -118.283608);
+                viewModel.startPlace = startPlace;
+                viewModel.stopPlace = stopPlace;
+        
+                _ = try! viewModel.loadingMapRoutingSignal!.asynchronouslyWaitUntilCompleted()
+                expect(viewModel.encodedRoutingPoints).to(equal(NO_ROUTE_KEY))
+            }
         }
     }
 }
-
-//
-//describe(@"reverse direction", ^{
-//    __block DTPlace *stopPlace = nil;
-//    __block DTPlace *startPlace = nil;
-//    
-//    beforeAll(^ {
-//        viewModel = [[MapRoutingViewModel alloc] init];
-//        startPlace = [[DTPlace alloc] init];
-//        stopPlace = [[DTPlace alloc] init];
-//        viewModel.startPlace = startPlace;
-//        viewModel.stopPlace = stopPlace;
-//    });
-//    
-//    it (@"Should assert that reverse start and stop place is ok", ^{
-//        NSError *error = nil;
-//        RACSignal *resultSignal = [viewModel.reverseDirectionCommand execute:nil];
-//        BOOL success = [resultSignal asynchronouslyWaitUntilCompleted:&error];
-//        expect(success).to.beTruthy();
-//        expect(error).to.beNil();
-//        expect(viewModel.startPlace).to.equal(stopPlace);
-//        expect(viewModel.stopPlace).to.equal(startPlace);
-//    });
-//});
-//
-//describe(@"Load map routing", ^{
-//    __block DTPlace *stopPlace = nil;
-//    __block DTPlace *startPlace = nil;
-//    
-//    beforeAll(^ {
-//        viewModel = [[MapRoutingViewModel alloc] init];
-//        viewModel.encodedRoutingPoints = NO_ROUTE_KEY;
-//        startPlace = [[DTPlace alloc] init];
-//        startPlace.coordinate = CLLocationCoordinate2DMake(21.001315, 105.817504);
-//        stopPlace = [[DTPlace alloc] init];
-//        stopPlace.coordinate = CLLocationCoordinate2DMake(20.972324, 105.884813);
-//    });
-//    
-//    it (@"Should assert that encodedRoutingPoints is found", ^{
-//        viewModel.startPlace = startPlace;
-//        viewModel.stopPlace = stopPlace;
-//        
-//        NSError *error = nil;
-//        BOOL success = [viewModel.loadingMapRoutingSignal asynchronouslyWaitUntilCompleted:&error];
-//        expect(success).to.beTruthy();
-//        expect(error).to.beNil();
-//        expect(viewModel.mapBounds).to.beTruthy();
-//        expect(viewModel.encodedRoutingPoints).notTo.equal(NO_ROUTE_KEY);
-//    });
-//    
-//    it (@"Should assert that encodedRoutingPoints is NO_ROUTE_KEY", ^{
-//        viewModel.transportMode = DTTransportingModeTransit;
-//        stopPlace.coordinate = CLLocationCoordinate2DMake(34.112603, -118.283608);
-//        viewModel.startPlace = startPlace;
-//        viewModel.stopPlace = stopPlace;
-//        
-//        NSError *error = nil;
-//        BOOL success = [viewModel.loadingMapRoutingSignal asynchronouslyWaitUntilCompleted:&error];
-//        expect(success).to.beTruthy();
-//        expect(error).to.beNil();
-//        expect(viewModel.encodedRoutingPoints).to.equal(NO_ROUTE_KEY);
-//    });
-//});
-//
-//SpecEnd
